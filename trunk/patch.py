c367154bfe6d6d7cb1d6dcfa9391aa0b2ec948d0
techtonik <techtonik@7155d8b8-9951-0410-8dfa-c5fb0ae76a41>
2011-03-16 10:23:05 +0000

    Update issue 15 Apply diff from Mercurial/Git patches (no renames/adds/deletes yet)

--- trunk/patch.py
+++ trunk/patch.py
@@ -509,10 +509,20 @@ class PatchSet(object):
     """
     errors = 0
     for i,p in enumerate(self.items):
-      if self.type == HG or self.type == GIT:
-        # TODO: find a way to work around /dev/null entries
-        # strip a/ or b/ prefix
-        pass#p.source = p.
+      if p.type in (HG, GIT):
+        # TODO: figure out how to deal with /dev/null entries
+        debug("stripping a/ and b/ prefixes")
+        if p.source != '/dev/null':
+          if not p.source.startswith("a/"):
+            warning("invalid source filename")
+          else:
+            p.source = p.source[2:]
+        if p.target != '/dev/null':
+          if not p.target.startswith("b/"):
+            warning("invalid target filename")
+          else:
+            p.target = p.target[2:]
+
       p.source = normpath(p.source)
       p.target = normpath(p.target)
 
