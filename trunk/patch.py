3b464b6198784c96f48bc024ff2c3fdc90def31a
techtonik <techtonik@7155d8b8-9951-0410-8dfa-c5fb0ae76a41>
2014-01-15 19:10:29 +0000

    Add alpha quality dump() function (commit early and often)

--- trunk/patch.py
+++ trunk/patch.py
@@ -1052,6 +1052,17 @@ class PatchSet(object):
     return True
 
 
+  def dump(self):
+    for p in self.items:
+      for headline in p.header:
+        print headline.rstrip('\n')
+      print '--- ' + p.source
+      print '+++ ' + p.target
+      for h in p.hunks:
+        print '@@ -%s,%s +%s,%s @@' % (h.startsrc, h.linessrc, h.starttgt, h.linestgt)
+        for line in h.text:
+          print line.rstrip('\n')
+
 
 if __name__ == "__main__":
   from optparse import OptionParser
