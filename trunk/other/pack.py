7bf8c2bd6b44cb505c5b527e61ce3e5d663ce62b
techtonik <techtonik@7155d8b8-9951-0410-8dfa-c5fb0ae76a41>
2015-02-05 06:18:09 +0000

    other/pack: Create .zip archive with patch.py

--- trunk/other/pack.py
+++ trunk/other/pack.py
@@ -17,13 +17,22 @@ def get_version(path):
       # __version__ = "0.9"
       return line.split('"')[1]
 
+def zipadd(archive, filename, newname):
+  '''Add filename to archive. `newname` is required. Otherwise
+     zipfile may create unsafe entries, such as "../patch.py" '''
+  import zipfile
+  zf = zipfile.ZipFile(archive, 'a', zipfile.ZIP_DEFLATED)
+  zf.write(filename, newname)
 
 if not sys.argv[1:]:
   sys.exit("usage: pack.py <module.py>")
 
 modpath = sys.argv[1]
-modname = os.path.basename(modpath)
+modname = os.path.basename(modpath)[:-3] # also strip extension
 version  = get_version(modpath)
 packname = modname + "-" + version + ".zip"
 print("[*] Packing %s into %s" % (modpath, packname))
+if os.path.exists(packname):
+  os.remove(packname)
+zipadd(packname, modpath, os.path.basename(modpath))
 
