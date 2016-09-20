b49da9f0c51bfd2f64eab25d65ca8924a28efb3c
Thomas Grainger <tom.grainger@procensus.com>
2016-09-20 15:34:58 +0100

    configure wheels Fixes #43

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
-from distutils.core import setup
+from setuptools import setup
+
+readme = open('README.rst').read()
 
 setup(
     name='patch',
-    version='1.16',
-    author='anatoly techtonik <techtonik@gmail.com>',
+    version='1.16.0',
+    author='Anatoly Techtonik',
+    autor_email='techtonik@gmail.com',
     url='https://github.com/techtonik/python-patch/',
-
     description='Patch utility to apply unified diffs',
     license='MIT',
-
     py_modules=['patch'],
-
+    long_description=readme,
     classifiers=[
-        'Classifier: Programming Language :: Python :: 2',
-        'Classifier: Programming Language :: Python :: 3',
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
+    entry_points={'console_scripts': 'patch = patch:main'},
+    zip_safe=True,
 )
